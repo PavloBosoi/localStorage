676f6df2538077f0c09fe65974d72238a40d2808
dark1910 <pashabos19@mail.ru>
2017-07-14 17:53:38 +0300

    add,clear all,reload

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(function (){
         var items = localStorage.getItem(key);
         $('.js-task-input').on('keyup',function(e){
             items = localStorage.getItem(key);
-            if(e.keyCode === 13){
+            if(e.keyCode === 13 && e.target.value !== ''){
                 if(items !== null){
                     tasks = items.split(',');
                     tasks.push($(this).val());
@@ -31,13 +31,27 @@ $(function (){
             tasks = [];
         });
 
+        inputItems(tasks);
+        removeItem(tasks);
+
         function inputItems(tasks){
             blockItems.empty();
             tasks.forEach(function(item){
-                blockItems.append('<span>'+item+'</span>');
+                blockItems.append('<span class="js-task-item">'+item+'</span>');
             });
         }
-        inputItems(tasks);
+
+        function removeItem(tasks){
+            console.log(tasks);
+            $(document).on('click','.js-task-item',function(e){
+                var item = $(e.target),
+                    itemText = item.text();
+                item.remove();
+                tasks.splice(tasks.indexOf(itemText),1);
+                localStorage.setItem(key,tasks);
+            });
+        }
+
     }
 
     addToLocalStorage();
